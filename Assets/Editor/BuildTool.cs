afed72a2b1d379892e96c142f35f2955e47850bb
xuzhipeng <1071556447@qq.com>
2022-07-11 21:31:44 +0800

    第一次提交

---
+++ Assets/Editor/BuildTool.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public static  class BuildTool
+{
+
+    public static string[] FindEnableScene()
+    {
+        List<string> scenes = new List<string>();
+        foreach (var sceneItem in EditorBuildSettings.scenes)
+        {
+            if (!sceneItem.enabled) continue;
+            scenes.Add(sceneItem.path);
+            Debug
+                .Log($"sceneItem.path:{sceneItem.path}");
+        }
+        return scenes.ToArray();
+    }
+
+    [MenuItem("Build/Build PC")]
+    public static void BuildPC()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = FindEnableScene();
+        buildPlayerOptions.locationPathName = "PCBuild/PCDemo.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        //BuildReport report =BuildPipeline.BuildPlayer(FindEnableScene(), Application.dataPath + "/../PCBuild/", BuildTarget.StandaloneWindows, BuildOptions.None);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+
+    [MenuItem("Build/Build WebGL")]
+    public static void BuildWebGL()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = FindEnableScene();
+        buildPlayerOptions.locationPathName = "WebGLBuild/packDemo";
+        buildPlayerOptions.target = BuildTarget.WebGL;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
---
+++ UnityPackage.bat
@@ -0,0 +1,9 @@
+@echo off
+ 
+
+set UNITY_PATH="C:\Program Files\Unity\Hub\Editor\2018.4.16f1\Editor\Unity.exe"
+set UNITY_PROJECT_PATH="D:\Documents\UnityProjects\AutoPackDEMO"
+set UNITY_METHOD_NAME="BuildTool.BuildPC"
+
+%UNITY_PATH% -quit -batchmode -projectPath %UNITY_PROJECT_PATH% -executeMethod %UNITY_METHOD_NAME% -logfile "log.txt"
+
